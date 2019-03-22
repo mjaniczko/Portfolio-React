bf580d86bf3fc7b4f53d7775fc3d079a358b21ed
Michał Janiczko <michaljaniczko1@gmail.com>
2019-03-22 09:26:51 +0100

    Creating 1st component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render () {
+        return (
+            <div>
+                <h1>Hello</h1>
+                <p>My name is Michal. I'm a junior software engineer.</p>
+                <p>I'm always looking forward to working on meaningful projects.</p>
+            </div>
+        )
+    }
+}
+
+export default App;
