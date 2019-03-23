5d73ef20e7c9666ee0043c9d3735a3683232f054
Michał Janiczko <michaljaniczko1@gmail.com>
2019-03-23 11:02:01 +0100

    Added file to export social profiles array

---
+++ src/data/socialProfiles.js
@@ -0,0 +1,23 @@
+import emailIcon from '../assets/email_icon';
+import githubIcon from '../assets/github_icon';
+import linkedinIcon from '../assets/linkedin_icon';
+
+const SOCIAL_PROFILES = [
+    {
+        id: 1,
+        link: 'mailto:michaljaniczko@gmail.com',
+        image: emailIcon
+    },
+    {
+        id: 2,
+        link: 'https://github.com/mjaniczko',
+        image: githubIcon
+    },
+    {
+        id: 3,
+        link: 'https://www.linkedin.com/in/micha%C5%82-janiczko-aa41b0152/',
+        image: linkedinIcon
+    }
+];
+
+export default SOCIAL_PROFILES;
