6a91f921139bec2359bd6332d6fc17b1d146a763
Michał Janiczko <michaljaniczko1@gmail.com>
2019-03-24 20:38:41 +0100

    Adding picture into page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Projects from './Projects';
 import SocialProfiles from './SocialProfiles';
+import './index.css';
+import profile from './assets/user.jpg';
 
 class App extends Component {
     //constructor () {
@@ -18,7 +20,8 @@ class App extends Component {
     render () {
         return (
             <div>
-                <h1>Hello</h1>
+				<img src={profile} alt='profile' className="profile" />
+                <h1>Hello!</h1>
                 <p>My name is Michal. I'm a junior software engineer.</p>
                 <p>I'm always looking forward to working on meaningful projects.</p>
                 {
