9229d6a29d537bc259aaf2b611a87e5da57baa68
Michał Janiczko <michaljaniczko1@gmail.com>
2019-03-24 20:39:54 +0100

    Added ';' at the end of imports

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,6 @@
-import project1 from '../assets/project1.png'
-import project2 from '../assets/project2.png'
-import project3 from '../assets/project3.png'
+import project1 from '../assets/project1.png';
+import project2 from '../assets/project2.png';
+import project3 from '../assets/project3.png';
 
 const PROJECTS = [
     {
