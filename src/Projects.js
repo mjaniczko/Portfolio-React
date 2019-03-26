d00e27cfc2a90ff77bd0852cb080b47f02b17df4
Michał Janiczko <michaljaniczko1@gmail.com>
2019-03-26 20:41:02 +0100

    refactor??

--- src/Projects.js
+++ src/Projects.js
@@ -2,36 +2,42 @@ import React, { Component } from 'react';
 import PROJECTS from './data/projects';
 
 class Project extends Component {
-    render () {
 
-        const { title, image, description, link } = this.props.project;
+	render () {
 
-        return (
-            <div style={{ display: 'inline-block', width: 300, margin: 10 }}>
-                <h3>{title}</h3>
-                <img src={image} alt='profile' style={{ width: 200, height: 120 }}/>
-                <p>{description}</p>
-                <a href={link}>{link}</a>
-            </div>
-        )
-    }
+	const { title, image, description, link } = this.props.project;
+
+	return (
+   	<div style={{ display: 'inline-block', width: 300, margin: 10 }}>
+      	<h3>{title}</h3>
+      	<img src={image} alt='profile' style={{ width: 200, height: 120 }}/>
+      	<p>{description}</p>
+      	<a href={link}>{link}</a>
+    	</div>
+  	)
+
+  	}
 }
 
 class Projects extends Component {
-    render () {
-        return (
-            <div>
-                <h2>Highlihted Projects</h2>
-                {
-                    PROJECTS.map(PROJECT => {
-                        return (
-                            <Project key={PROJECT.id} project={PROJECT} />
-                        );
-                    })
-                }
-            </div>
-        )
-    };
+
+   render () {
+
+   return (
+
+      <div>
+        <h2>Highlihted Projects</h2>
+        {
+          PROJECTS.map(PROJECT => {
+            return (
+              <Project key={PROJECT.id} project={PROJECT} />
+            );
+         })
+        }
+      </div>
+
+   )
+   };
 };
 
 export default Projects;
