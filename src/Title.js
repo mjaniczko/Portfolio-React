540360698d57da09b4ff20fc608fa81e3e6a2476
Michał Janiczko <michaljaniczko1@gmail.com>
2019-04-06 22:02:28 +0200

    Added fade-in and fade-out css classes with logic

--- src/Title.js
+++ src/Title.js
@@ -7,29 +7,35 @@ const TITLES = [
 ];
 
 class Title extends Component {
-  state = { titleIndex: 0};
+  state = { titleIndex: 0, fadeIn: true };
 
   componentDidMount() {
+    this.timeout = setTimeout(() => this.setState({ fadeIn: false }), 2000);
     this.animateTitles();
   }
 
   componentWillUnmount() {
     clearInterval(this.titleInterval);
+    clearTimeout(this.timeout);
   };
 
   animateTitles = () => {
     this.titleInterval = setInterval(() => {
       const titleIndex = (this.state.titleIndex + 1) % TITLES.length;
 
-      this.setState({ titleIndex });
+      this.setState({ titleIndex, fadeIn: true });
+
+      this.timeout = setTimeout(() => this.setState({ fadeIn: false }), 2000);
     }, 4000);
   }
 
   render() {
+    const { fadeIn, titleIndex} = this.state;
+
     const title = TITLES[this.state.titleIndex];
 
     return (
-      <p>I am {title}</p>
+      <p className={ fadeIn ? 'title-fade-in' : 'title-fade-out'}>I am {title}</p>
     )
   }
 }
