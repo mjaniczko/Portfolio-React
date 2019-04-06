fcf1501ab46a4068830032471ef0aa8d279e1c68
Michał Janiczko <michaljaniczko1@gmail.com>
2019-04-06 21:33:19 +0200

    Animated titles

--- src/Title.js
+++ src/Title.js
@@ -9,6 +9,20 @@ const TITLES = [
 class Title extends Component {
   state = { titleIndex: 0};
 
+  componentDidMount() {
+    console.log('Title component has mounted');
+
+    this.animateTitles();
+  }
+
+  animateTitles = () => {
+    setInterval(() => {
+      const titleIndex = (this.state.titleIndex + 1) % TITLES.length;
+
+      this.setState({ titleIndex });
+    }, 4000);
+  }
+
   render() {
     const title = TITLES[this.state.titleIndex];
 
