7705f77d6de8b9d09444511ac59a07b6bee766c4
Michał Janiczko <michaljaniczko1@gmail.com>
2019-04-09 09:07:00 +0200

    Added routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Switch, Route } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
 import App from './components/App';
+import Jokes from './components/Jokes';
 import './index.css';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+
+ReactDOM.render(
+  <Router history={createBrowserHistory()}>
+    <Switch>
+      <Route exact path='/' component={App} />
+      <Route path='/jokes' component={Jokes} />
+    </Switch>
+  </Router>,
+  document.getElementById('root')
+  );
