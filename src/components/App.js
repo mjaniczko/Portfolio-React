982f1b5bd986fd32d0ec008035cf6eab31df5f91
Michał Janiczko <michaljaniczko1@gmail.com>
2019-04-09 09:39:23 +0200

    Removing Header

--- src/components/App.js
+++ src/components/App.js
@@ -2,52 +2,48 @@ import React, { Component } from 'react';
 import Projects from './Projects';
 import SocialProfiles from './SocialProfiles';
 import Title from './Title';
-import Header from './Header';
 import profile from '../assets/user.jpg';
 
 class App extends Component {
-    //constructor () {
-        //    super();
-        //    this.state = { displayBio: false };
-        //    this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
-        //};
-    state = { displayBio: false }
-
-    // togleDisplayBio () {this.set...}
-    toggleDisplayBio = () => {
-        this.setState({ displayBio: !this.state.displayBio });
-    };
-
-    render () {
-        return (
-            <div>
-                <Header />
+  //constructor () {
+      //    super();
+      //    this.state = { displayBio: false };
+      //    this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
+      //};
+  state = { displayBio: false }
+  // togleDisplayBio () {this.set...}
+  toggleDisplayBio = () => {
+      this.setState({ displayBio: !this.state.displayBio });
+  };
+  render () {
+    return (
+      <div>
 				<img src={profile} alt='profile' className="profile" />
-                <h1>Hello!</h1>
-                <p>My name is Michal.</p>
-                <Title />
-                <p>I'm always looking forward to working on meaningful projects.</p>
-                {
-                    this.state.displayBio ? (
-                        <div>
-                            <p>I live in Cracow, and code every day</p>
-                            <p>My favorite language is JavaScript, and i think React.js is awesome</p>
-                            <p>Besides coding, I also love music, sports and good food :D</p>
-                            <button onClick={this.toggleDisplayBio}>Show less</button>
-                        </div>
-                    ) : (
-                        <div>
-                            <button onClick={this.toggleDisplayBio}>Read more</button>
-                        </div>
-                    )
-                }
-                <hr/>
-                <Projects/>
-                <hr />
-                <SocialProfiles />
-            </div>
-        );
-    };
+          <h1>Hello!</h1>
+          <p>My name is Michal.</p>
+          <Title />
+          <p>I'm always looking forward to working on meaningful projects.</p>
+          {
+            this.state.displayBio ? (
+              <div>
+                <p>I live in Cracow, and code every day</p>
+                <p>My favorite language is JavaScript, and i think React.js is awesome</p>
+                <p>Besides coding, I also love music, sports and good food :D</p>
+                <button onClick={this.toggleDisplayBio}>Show less</button>
+              </div>
+            ) : (
+              <div>
+                <button onClick={this.toggleDisplayBio}>Read more</button>
+              </div>
+            )
+          }
+          <hr/>
+          <Projects/>
+          <hr />
+          <SocialProfiles />
+      </div>
+    );
+  };
 };
 
 export default App;
