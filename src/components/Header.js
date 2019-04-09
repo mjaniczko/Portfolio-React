7340726033271c2a082a95210aad67cb9b857747
Michał Janiczko <michaljaniczko1@gmail.com>
2019-04-09 09:38:58 +0200

    Change Header into higher order component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = () => {
+const Header = ({ children }) => {
+
+  const style = {
+    display: 'inline-block',
+    margin: 10,
+    marginBottom: 30
+  };
   return (
     <div>
-      <h3><Link to="/">Home</Link></h3>
-      <h3><Link to="/jokes">Jokes</Link></h3>
+      <div>
+        <h3 style={style}><Link to="/">Home</Link></h3>
+        <h3 style={style}><Link to="/jokes">Jokes</Link></h3>
+      </div>
+      { children }
     </div>
   )
 }
